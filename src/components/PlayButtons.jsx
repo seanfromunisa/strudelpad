eb0b4beedfc72009f38170d807b69e2c7d0d496f
Sean Fuller <fulsd003@mymail.unisa.edu.au>
2025-11-07 06:21:32 +1030

    Seperating button control and preprocess text components into jsx files

---
+++ src/components/PlayButtons.jsx
@@ -0,0 +1,14 @@
+function PlayButtons() {
+    return (
+        <>
+            <div className="btn-group" role="group" aria-label="Basic mixed styles example">
+                <button id="play" className="btn btn-outline-primary">Play</button>
+                <button id="stop" className="btn btn-outline-primary">Stop</button>
+            </div>
+        </>
+
+
+  );
+}
+
+export default PlayButtons;
