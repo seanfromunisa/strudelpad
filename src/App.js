0dffe5eb87ebf14255d2c0acb3eb9f00b22eba5d
Sean Fuller <fulsd003@mymail.unisa.edu.au>
2026-01-23 23:53:31 +1030

    Moved song text word replacement to separate logic file
    The song text is now refactored in a separate logic file and passed back, updating the outputText variable in App.js

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ import Volume from './components/Volume';
 import CPM from './components/CPM';
 import RadioDJ from './components/RadioDJ';
 import SaveLoadButtons from './components/SaveLoadButtons';
+import InstrumentCheckLogic from './InstrumentCheckLogic';
 
 //Establishing globalEditor variable
 let globalEditor = null;
@@ -36,39 +37,8 @@ export default function StrudelDemo() {
             outputText = stranger_tune;
         }
 
-        //Replace volume, cpm, and drum type variables across the song text with their set values
-        outputText = outputText.replaceAll("{VOLUME}", volume);
-        outputText = outputText.replaceAll("{CPM}", cpm);
-        outputText = outputText.replaceAll("{DRUMTYPE}", drumType);
-
-        //Check which radio buttons have been selected. If any are selected, keep their values empty. For the rest, replace in song text with "_" to mute their respective instrument
-        if (!b1Checked) {
-            outputText = outputText.replaceAll("{B1}", "_");
-        }
-        else {
-            outputText = outputText.replaceAll("{B1}", "");
-        }
-
-        if (!a1Checked) {
-            outputText = outputText.replaceAll("{A1}", "_");
-        }
-        else {
-            outputText = outputText.replaceAll("{A1}", "");
-        }
-
-        if (!d1Checked) {
-            outputText = outputText.replaceAll("{D1}", "_");
-        }
-        else {
-            outputText = outputText.replaceAll("{D1}", "");
-        }
-
-        if (!d2Checked) {
-            outputText = outputText.replaceAll("{D2}", "_");
-        }
-        else {
-            outputText = outputText.replaceAll("{D2}", "");
-        }
+        //Replace variables in the song text to reflect the users instrument settings
+        outputText = InstrumentCheckLogic(outputText, volume, cpm, drumType, b1Checked, a1Checked, d1Checked, d2Checked);
 
         //Having refactored all of the song text, process it into strudel and execute
         globalEditor.setCode(outputText);
