f294d1ffbbd9c9290ace376ccf79306f75e18539
Sean Fuller <fulsd003@mymail.unisa.edu.au>
2026-01-25 23:40:25 +1030

    Rearranged the page and added accordian
    The preprocess text area and the output are now within the same column, held in an accordion component that stores these elements under two separate tabs. Could not relegate the accordion to a separate file as this would interfere with Strudel

--- src/App.js
+++ src/App.js
@@ -185,11 +185,40 @@ return (
         <main>
 
             <div className="container-fluid">
-                <div className="row">
-                    <div className="col-md-8" style={{ maxHeight: '50vh', overflowY: 'auto' }}>
-                        <PreprocessTextArea defaultValue={songText} onChange={(e) => setSongText(e.target.value)} />
+                <div className="col-md-8" style={{ maxHeight: '50vh', overflowY: 'auto' }}>
+                    <div class="accordion" id="strudelAccordion">
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="panelsStayOpen-headingOne">
+                                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+                                    Preprocess Text
+                                </button>
+                            </h2>
+                            <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
+                                <div class="accordion-body">
+                                    <PreprocessTextArea defaultValue={songText} onChange={(e) => setSongText(e.target.value)} />
+                                </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+                                    Strudel Player
+                                </button>
+                            </h2>
+                            <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
+                                <div class="accordion-body">
+                                    <div id="editor" />
+                                    <div id="output" />
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div className="col-md-4">
+                </div>
+
+                <div className="col-md-4" style={{ maxHeight: '50vh', overflowY: 'auto' }}>
+                    <div className="row">
+                    </div>
+                    <div>
                         <nav>
                             <div>
                                 <ThemeSelect
@@ -206,15 +235,6 @@ return (
                                 <PlayButtons onPlay={() => { setState("play"); handlePlay() }} onStop={() => { setState("stop"); handleStop() }} />
                             </div>
                         </nav>
-                    </div>
-                </div>
-
-                <div className="row">
-                    <div className="col-md-8" style={{ maxHeight: '50vh', overflowY: 'auto' }}>
-                        <div id="editor" />
-                        <div id="output" />
-                    </div>
-                    <div className="col-md-4">
 
                         <div className="row">
                             <CPM defaultValue={cpm} onChange={(e) => setCpm(e.target.value)} />
